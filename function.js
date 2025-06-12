4cc93fdae2ea03898e647d0a95e84f2d51367e72
JuaniTeruya <jteruya@pioix.edu.ar>
2025-06-12 08:39:01 -0300

    Hasta el 14 o 15

--- function.js
+++ function.js
@@ -13,7 +13,55 @@ function findCajas(idCliente) {
         for (let j = 0; j < clients[i].savingsBanks.length; j++) {
             cajasUser.push(clients[i].savingsBanks[j])
         }
-        console.log(cajasUser)
+        return cajasUser
     }
 }
-findCajas(1)
+
+function findTarjetaDebitoCliente(idCliente) {
+    let i = findClient(idCliente)
+    let tarjetasDebito = []
+    if (i != -1) {
+        for (let j = 0; j < clients[i].savingsBanks.length; j++) {
+            for (let x = 0; x < clients[i].savingsBanks[j].debitCards.length; x++) {
+                tarjetasDebito.push(clients[i].savingsBanks[j].debitCards[x])
+            }
+        }
+        return tarjetasDebito
+    }
+}
+
+function findTarjetaDebito(idTarjeta) {
+
+    for (let i = 0; clients.length; i++) {
+        for (let j = 0; j < clients[i].savingsBanks.length; j++) {
+            for (let x = 0; x < clients[i].savingsBanks[j].debitCards.length; x++) {
+                if (clients[i].savingsBanks[j].debitCards[x].id == idTarjeta) {
+                    return clients[i].savingsBanks[j].debitCards[x]
+                }
+            }
+        }
+    }
+}
+
+
+function findTarjetaCreditoCliinte(idCliente) {
+    let i = findClient(idCliente)
+    let tarjetasCredito = []
+    if (i != -1) {
+        for (let j = 0; j < clients[i].creditCards.length; j++) {
+            tarjetasCredito.push(clients[i].creditCards[j])
+        }
+        return  tarjetasCredito
+    }
+}
+
+function findTarjetaCredito(idTarjeta) {
+
+    for (let i = 0; clients.length; i++) {
+        for (let j = 0; j < clients[i].creditCards.length; j++) {
+                if (clients[i].creditCards[j].id == idTarjeta) {
+                    return clients[i].creditCards[j]
+                }
+            }
+        }
+    }
